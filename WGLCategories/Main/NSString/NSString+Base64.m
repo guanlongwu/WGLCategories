616de07b248aad2f3057cbf589ebc44c4ffc5ca5
wuguanlong <guanlongwu@qq.com>
2019-03-14 18:12:45 +0800

    This is a uikit categories tool

---
+++ WGLCategories/Main/NSString/NSString+Base64.m
@@ -0,0 +1,40 @@
+//
+//  NSString+Base64.m
+//  WGLUtils
+//
+//  Created by wugl on 2019/3/4.
+//  Copyright © 2019年 WGLKit. All rights reserved.
+//
+
+#import "NSString+Base64.h"
+
+@implementation NSString (Base64)
+
+/**
+ *  将普通字符串转换成base64字符串
+ *
+ *  @return base64字符串
+ */
+- (NSString *)toBase64String {
+    
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *base64String = [data base64EncodedStringWithOptions:0];
+    
+    return base64String;
+}
+
+
+/**
+ *  将base64字符串转换成普通字符串
+ *
+ *  @return 普通字符串
+ */
++ (NSString *)textFromBase64String {
+    
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:self options:0];
+    NSString *text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    return text;
+}
+
+@end
